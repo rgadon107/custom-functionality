901909a5b9fbbefe40008fb584162ccce4a5b021
Robert Gadon <rgadon107@gmail.com>
2026-03-12 23:48:39 -0400

    Initial commit of `/src/controller.php` file.

---
+++ src/controller.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Main controller file.
+ *
+ * Handle the routing and loading of modular logic files stored within the `/src` directory.
+ *
+ */
+namespace gardenClubOfMpls\CustomFunctionalityPlugin\Source;
