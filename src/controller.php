07f7208237938cbd62261e7a3c2f731e09d961db
Robert Gadon <rgadon107@gmail.com>
2026-03-27 00:02:11 -0500

    Load directory modules and register plugin block patterns.

--- src/controller.php
+++ src/controller.php
@@ -6,3 +6,29 @@
  *
  */
 namespace gardenClubOfMpls\CustomFunctionalityPlugin\Source;
+
+// Load the directory modules.
+require_once __DIR__ . '/asset/handler.php';
+require_once __DIR__ . '/shortcodes/expire-content.php';
+
+/**
+ * Register the plugin block patterns.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+add_action('init', function (): void {
+
+	$pattern_path = __DIR__ . '/patterns/registration-closed.php';
+
+	if (file_exists($pattern_path)) {
+		register_block_pattern(
+			'registration-closed-message',
+			array(
+				'title' 	=>  'Registration Closed Notice',
+				'content' 	=> 	file_get_contents($pattern_path)
+			)
+		);
+	}
+});
