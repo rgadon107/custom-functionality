dc2aa1e581f393522ba90338b60bc22c1eafe3b6
Robert Gadon <rgadon107@gmail.com>
2026-03-27 23:53:57 -0500

    Update pattern files.

---
+++ src/patterns/message-after-registration-stop.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Notice - After Registration Stop
+ * Slug: notice-after-registration-stop
+ * Categories: featured, message-after-registration-stop
+ * Description: Style a message displayed after an event registration stops.
+ * Keywords: featured, message, after registration
+ * Viewport Width: 400
+ */
+?>
+<!-- wp:paragraph { "className": "message-after-registration-stop", "placeholder": "Registration for this event is now closed." } -->
+<p class="message-after-registration-stop"></p>
+<!-- /wp:paragraph -->
---
+++ src/patterns/message-before-registration-start.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Notice - Before Registration Start
+ * Slug: notice-before-registration-start
+ * Categories: featured, message-before-registration-start
+ * Description: Style a message displayed before an event registration starts.
+ * Keywords: featured, message, before registration
+ * Viewport Width: 400
+ */
+?>
+<!-- wp:paragragph { "className": "message-before-registration-start", "placeholder": "Registration for this event will start on {MM-DD-YYYY}. Please come back then to register {for dinner, or whatever event it is}." } -->
+<p class="message-before-registration-start"></p>
+<!-- /wp:paragraph -->
