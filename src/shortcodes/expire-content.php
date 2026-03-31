93665d74afd5b39f5aa2b53b086927d64dcb3949
Robert Gadon <rgadon107@gmail.com>
2026-03-31 11:47:24 -0500

    Register 2 shortcodes ( [expire] and [expire_inner] ) to one custom callback so that [expire] can wrap [expire_inner] without interfering with the latter.

--- src/shortcodes/expire-content.php
+++ src/shortcodes/expire-content.php
@@ -2,10 +2,15 @@
 
 namespace gardenClubOfMpls\CustomFunctionalityPlugin\Source;
 
+add_shortcode( 'expire', __NAMESPACE__ . '\expire_shortcode_handler' );
+add_shortcode( 'expire_inner', __NAMESPACE__ . '\expire_shortcode_handler');
 /**
- * Shortcode: [expire]
+ * Shortcodes: [expire] and [expire_inner]
  *
  * Shortcode to display content within a scheduled date and time window.
+ *
+ * The [expire] shortcode can wrap the [expire_inner] shortcode without interfering with the latter's
+ * 	start and stop times and wrapped content.
  *
  * Provides a "Pending -> Active -> Expired" lifecycle for wrapped content.
  * Supported attributes within the $user_defined_attributes array:
@@ -28,7 +33,7 @@ namespace gardenClubOfMpls\CustomFunctionalityPlugin\Source;
  *
  * @return string The filtered content if within the time window, or a notice/empty string if not.
  */
-add_shortcode( 'expire', function( array|string $user_defined_attributes, ?string $content = null ): string {
+function expire_shortcode_handler( array|string $user_defined_attributes, ?string $content = null ): string {
 
 	// Load the shortcode styles only on posts and pages where the shortcode is used.
 	wp_enqueue_style( 'event-registration-notice-styles' );
@@ -46,7 +51,7 @@ add_shortcode( 'expire', function( array|string $user_defined_attributes, ?strin
 	$stop_timestamp    = strtotime( $final_settings['stop_date_and_time'] );
 	$current_timestamp = current_time( 'timestamp' );
 
-	// STAGE 1: Before the shortcode start time, display 'pre_start_message'.
+	// STAGE 1: PENDING: Before the shortcode start time, display 'pre_start_message'.
 	if ( $current_timestamp < $start_timestamp ) {
 		if ( ! empty( $final_settings['pre_start_message'] ) ) {
 			return sprintf(
@@ -56,12 +61,12 @@ add_shortcode( 'expire', function( array|string $user_defined_attributes, ?strin
 		}
 	}
 
-	// STAGE 2: Shortcode is active. Display content wrapped by shortcode.
+	// STAGE 2: ACTIVE: Display content wrapped by shortcode.
 	if ( $current_timestamp >= $start_timestamp && $current_timestamp < $stop_timestamp ) {
 		return do_shortcode( $content ?? '' );
 	}
 
-	// STAGE 3: After shortcode stop time, display 'message'.
+	// STAGE 3: EXPIRED: After shortcode stop time, display 'message'.
 	if ( $current_timestamp >= $stop_timestamp ) {
 		if ( ! empty( $final_settings['message'] ) ) {
 			return sprintf(
@@ -72,4 +77,4 @@ add_shortcode( 'expire', function( array|string $user_defined_attributes, ?strin
 	}
 
 	return '';
-});
+}
