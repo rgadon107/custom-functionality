8cf3300748ce6ea69f6f6d6cda8f37a54ae69765
Robert Gadon <rgadon107@gmail.com>
2026-05-13 00:51:50 -0500

    Eliminated leading whitespace before HTML delimiters and newline at end of each file.

--- src/patterns/message-before-registration-start.php
+++ src/patterns/message-before-registration-start.php
@@ -7,7 +7,6 @@
  * Keywords: featured, message, before registration
  * Viewport Width: 400
  */
-?>
-<!-- wp:paragraph { "className": "message-before-registration-start", "placeholder": "Registration for this event will start on {MM-DD-YYYY}. Please come back then to register {for dinner, or whatever event it is}." } -->
+?><!-- wp:paragraph { "className": "message-before-registration-start", "placeholder": "Registration for this event will start on {MM-DD-YYYY}. Please come back then to register {for dinner, or whatever event it is}." } -->
 <p class="message-before-registration-start"></p>
 <!-- /wp:paragraph -->
