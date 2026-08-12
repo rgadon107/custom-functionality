7230a71cf84cfe71bedf085fca290ef72bee0805
Robert Gadon <rgadon107@gmail.com>
2026-08-12 17:15:51 -0500

    Refactor file and call `$config_path`.

--- src/shortcodes/member-photo-directory.php
+++ src/shortcodes/member-photo-directory.php
@@ -10,26 +10,28 @@ add_shortcode( 'member_photo_directory', __NAMESPACE__ . '\member_photo_director
  * Shortcode: [member_photo_directory]
  *
  * Shortcode to display the Garden Club of Minneapolis member photo directory.
- * Returns an embedded PDF file view that responds to the user's screen width.
- * * @param array|string $user_defined_attributes Shortcode attributes passed by WordPress.
- * @type string $desktop_url The URL of the PDF file for desktop screens.
- * @type string $tablet_url  The URL of the PDF file for tablet screens.
- * @type string $mobile_url  The URL of the PDF file for mobile screens.
  *
- * @return string The HTML template view to render the member photo directory file.
+ * @param array|string $user_defined_attributes Shortcode attributes passed by WordPress.
+ *
+ * @return string The HTML template view for the PDF viewer.
  */
 function member_photo_directory_shortcode_handler( array|string $user_defined_attributes ): string {
 	// Enqueue scripts and styles ONLY on the pages displaying this block
 	wp_enqueue_style( 'member-photo-directory-viewer-styles' );
 	wp_enqueue_script( 'member-photo-directory-loader' );
 
-	// Merge user attributes with default values in a single call
+	// 1. Fetch default configuration array from updated configuration/shortcodes path
+	$config_path = _get_plugin_directory() . '/src/configuration/shortcodes/member-photo-directory.php';
+
+	$defaults = file_exists( $config_path ) ? require $config_path : array(
+		'desktop_url' => '',
+		'tablet_url'  => '',
+		'mobile_url'  => '',
+	);
+
+	// 2. Merge shortcode attributes with the configuration defaults
 	$atts = shortcode_atts(
-		array(
-			'desktop_url' => '',
-			'tablet_url'  => '',
-			'mobile_url'  => '',
-		),
+		$defaults,
 		$user_defined_attributes,
 		'member_photo_directory'
 	);
@@ -38,6 +40,7 @@ function member_photo_directory_shortcode_handler( array|string $user_defined_at
 	$tablet_pdf_url  = $atts['tablet_url'];
 	$mobile_pdf_url  = $atts['mobile_url'];
 
+	// 3. Render HTML view template
 	ob_start();
 
 	$template_path = _get_plugin_directory() . '/src/templates/member-photo-directory-view.php';
