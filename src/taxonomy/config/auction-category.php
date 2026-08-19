890810db9e8ccdae0dd60fabca648ddd604f71b9
Robert Gadon <rgadon107@gmail.com>
2026-08-18 21:22:54 -0500

    Add the `auction_category` taxonomy for the object_type ( custom post-type ) named `auction_item`.

--- src/taxonomy/config/auction-category.php
+++ src/taxonomy/config/auction-category.php
@@ -1,1 +1,31 @@
 <?php
+
+namespace gardenClubOfMpls\CustomFunctionalityPlugin\Source\Taxonomy;
+
+use function gardenClubOfMpls\CustomFunctionalityPlugin\Source\Custom\build_entity_labels;
+
+return [
+	'taxonomy'  => 'auction_category',
+	'object_type' => [ 'auction_item' ],
+	'args'      => [
+		'hierarchical'      => true,
+		'labels'            => build_entity_labels(
+			'Auction Category',
+			'Auction Categories',
+			'taxonomy',
+			'custom-functionality',
+			[
+				'menu_name' => __(
+					'Categories',
+					'custom-functionality'
+				),
+			]
+		),
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'          => true,
+		'rewrite'           => [ 'slug' => 'auction-category' ],
+		'show_in_rest'      => true,
+		'rest_base'         => 'auction_category',
+	],
+];
